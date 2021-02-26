e7172376c1eb6e22feffd11618a89553ee64e652
jomal <jomal1236@gmail.com>
2021-02-26 19:52:39 +0100

    Update

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from selenium.webdriver import Firefox
-import multiprocessing
+import os
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
+
 import requests,json
 from bs4 import BeautifulSoup
 class Book():
-    def __init__(self, session:requests.Session, id, name=None):
+    def __init__(self,dir_prefix, session:requests.Session, id, name=None):
+        self.view_url = 'http://wbsl.gov.in/bookReader.action?bookId={id}'
         self.session = session
+        self.in_progress_dir= f'{dir_prefix}_in_progres'
+        self.done_dir=f'{dir_prefix}_done'
         self.id = id
+        self.short_name = None
         self.name = name
         self.head_number = None
         self.author = None
@@ -23,7 +29,7 @@ class Book():
         self.meta_url = 'http://wbsl.gov.in/bookSearchResult.action?bookId={id}'
         self.image_url = 'http://wbsl.gov.in/Books/{disk_name}/{title}/PTIFF/{image}'
         self.image_list = []
-    def get_info_from_meta(self):
+    def get_info_from_meta(self)->None:
         bs = BeautifulSoup(self.session.get(self.meta_url.format(id=self.id)).content,'html.parser')
         trs = bs.find_all('tr')[1:]
         for el in trs:
@@ -33,7 +39,10 @@ class Book():
             if name == 'Head Number:\xa0':
                 self.head_number=value
             elif name == 'Title:\xa0' and self.name==None:
+                while value[-1]==' ':
+                    value=value[:-1]
                 self.name=value
+                self.short_name = value[:64]
             elif name == 'Author(s):\xa0':
                 self.author = value
             elif name == 'Description:\xa0':
@@ -58,15 +67,44 @@ class Book():
                 self.disk_name = value
             elif name == 'Source:\xa0':
                 self.source = value
-    def generate_imgs_names(self):
-        name=f'num.pdf'
-        while len(name)<8:
-            name = '0'+name
-        self.image_list.append(name)
+    def generate_imgs_names(self)->None:
+        for num in range(1,int(self.pages)+1):
+            name=str(num)
+            while len(name)<8:
+                name = '0'+name
+            self.image_list.append(name+'.jpg')
+    def get_url(self)->str:
+        return self.view_url.format(id=self.id)
+    def get_img_list(self)->list:
+        data =[]
+        for el in self.image_list:
+            data.append(self.image_url.format(disk_name=self.disk_name,title=self.name,image=el))
+        return data
+    def download(self)->None:
+        self.__make_dir()
+        for el in self.image_list:
+            url = self.image_url.format(disk_name=self.disk_name,title=self.name,image=el)
+            url = url.replace(' ','%20')
+            open(f'{self.in_progress_dir}/{self.short_name}/{el}', 'wb').write(session.get(url).content)
+    def async_download(self):
+        self.__make_dir()
 
 
-
-
-
+    def make_pdf(self)->None:
+        pass
+    def __make_dir(self):
+        try:
+            os.mkdir(self.in_progress_dir)
+        except FileExistsError:
+            pass
+        try:
+            os.mkdir(f'{self.in_progress_dir}/{self.short_name}')
+        except FileExistsError:
+            pass
 if __name__ == '__main__':
-    wb = Firefox()
+    ids = [x for x in range(1,22000)]
+    session = requests.session()
+    book = Book('./pdf',session, '13992')
+    book.get_info_from_meta()
+    book.generate_imgs_names()
+    book.download()
